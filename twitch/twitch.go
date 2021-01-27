cf87409a867c9238ae25e9a2a1c6372f09360b74
jybp <2850825+jybp@users.noreply.github.com>
2021-01-27 22:44:06 +0100

    Use GQL api

--- twitch/twitch.go
+++ twitch/twitch.go
@@ -6,10 +6,10 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/http/httputil"
 	"net/url"
 	"path"
 	"strings"
-	"time"
 
 	"github.com/pkg/errors"
 )
@@ -30,9 +30,6 @@ func ID(URL string) (string, error) {
 	return id, nil
 }
 
-// https://dev.twitch.tv/docs/api/reference/
-// https://github.com/videolan/vlc/blob/0b018b348f47cda82863809ab0385cb993c8aa33/share/lua/playlist/twitch.lua#L81
-
 // Client manages communication with the twitch API.
 type Client struct {
 	client      *http.Client
@@ -43,7 +40,7 @@ type Client struct {
 
 // New returns a new twitch API client.
 func New(client *http.Client, clientID string) Client {
-	return Client{client, clientID, "https://api.twitch.tv/", "http://usher.twitch.tv/"}
+	return Client{client, clientID, "https://gql.twitch.tv/gql", "http://usher.twitch.tv/"}
 }
 
 // Custom returns a new twitch API client with custom API endpoints
@@ -51,35 +48,52 @@ func Custom(client *http.Client, clientID, apiURL, usherAPIURL string) Client {
 	return Client{client, clientID, apiURL, usherAPIURL}
 }
 
-type token struct {
-	Token string `json:"token"`
-	Sig   string `json:"sig"`
-}
-
-func (c *Client) vodToken(ctx context.Context, id string) (token, error) {
-	u := fmt.Sprintf("%sapi/vods/%s/access_token?client_id=%s", c.apiURL, id, c.clientID)
-	resp, err := c.client.Get(u)
+func (c *Client) vodToken(ctx context.Context, id string) (token, sig string, err error) {
+	gqlPayload := `{"operationName":"PlaybackAccessToken_Template","query":"query PlaybackAccessToken_Template($login: String!, $isLive: Boolean!, $vodID: ID!, $isVod: Boolean!, $playerType: String!) {  streamPlaybackAccessToken(channelName: $login, params: {platform: \"web\", playerBackend: \"mediaplayer\", playerType: $playerType}) @include(if: $isLive) {    value    signature    __typename  }  videoPlaybackAccessToken(id: $vodID, params: {platform: \"web\", playerBackend: \"mediaplayer\", playerType: $playerType}) @include(if: $isVod) {    value    signature    __typename  }}","variables":{"isLive":false,"login":"","isVod":true,"vodID":"%s","playerType":"site"}}`
+	body := strings.NewReader(fmt.Sprintf(gqlPayload, id))
+	req, err := http.NewRequest(http.MethodPost, c.apiURL, body)
 	if err != nil {
-		return token{}, errors.WithStack(err)
+		return "", "", errors.WithStack(err)
+	}
+	req.Header.Set("Client-Id", c.clientID)
+	dump, err := httputil.DumpRequestOut(req, true)
+	if err != nil {
+		return "", "", errors.WithStack(err)
+	}
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return "", "", errors.Errorf("%v\n%s", err, string(dump))
 	}
 	defer resp.Body.Close()
 	if s := resp.StatusCode; s < 200 || s >= 300 {
-		b, _ := ioutil.ReadAll(resp.Body)
-		return token{}, errors.Errorf("%d\n%s\n%s", s, u, string(b))
+		return "", "", errors.Errorf("invalid status code %d\n%s", s, string(dump))
 	}
-	var t token
-	return t, errors.WithStack(json.NewDecoder(resp.Body).Decode(&t))
+
+	type respPayload struct {
+		Data struct {
+			VideoPlaybackAccessToken struct {
+				Value     string `json:"value"`
+				Signature string `json:"signature"`
+			} `json:"videoPlaybackAccessToken"`
+		} `json:"data"`
+	}
+
+	var p respPayload
+	if err := json.NewDecoder(resp.Body).Decode(&p); err != nil {
+		return "", "", errors.Errorf("%v\n%s", err, string(dump))
+	}
+	return p.Data.VideoPlaybackAccessToken.Value, p.Data.VideoPlaybackAccessToken.Signature, nil
 }
 
 // M3U8 retrieves the M3U8 file of a specific VOD.
 func (c *Client) M3U8(ctx context.Context, id string) ([]byte, error) {
-	tok, err := c.vodToken(ctx, id)
+	tok, sig, err := c.vodToken(ctx, id)
 	if err != nil {
 		return nil, errors.WithStack(err)
 	}
 
 	u := fmt.Sprintf("%svod/%s?nauth=%s&nauthsig=%s&allow_audio_only=true&allow_source=true",
-		c.usherAPIURL, id, tok.Token, tok.Sig)
+		c.usherAPIURL, id, tok, sig)
 	resp, err := c.client.Get(u)
 	if err != nil {
 		return nil, errors.WithStack(err)
@@ -94,52 +108,42 @@ func (c *Client) M3U8(ctx context.Context, id string) ([]byte, error) {
 
 // VOD describes a twitch VOD.
 type VOD struct {
-	ID           string    `json:"id"`
-	UserID       string    `json:"user_id"`
-	UserName     string    `json:"user_name"`
-	Title        string    `json:"title"`
-	Description  string    `json:"description"`
-	CreatedAt    time.Time `json:"created_at"`
-	PublishedAt  time.Time `json:"published_at"`
-	URL          string    `json:"url"`
-	ThumbnailURL string    `json:"thumbnail_url"`
-	Viewable     string    `json:"viewable"`
-	ViewCount    int       `json:"view_count"`
-	Language     string    `json:"language"`
-	Type         string    `json:"type"`
-	Duration     string    `json:"duration"`
-}
-
-type data struct {
-	Data       []VOD `json:"data"`
-	Pagination struct {
-		Cursor string `json:"cursor"`
-	} `json:"pagination"`
+	Title string
 }
 
 // VOD retrieves the video informations of a specific VOD.
 func (c *Client) VOD(ctx context.Context, id string) (VOD, error) {
-	u := fmt.Sprintf("%shelix/videos?id=%s", c.apiURL, id)
-	req, err := http.NewRequest(http.MethodGet, u, nil)
+	gqlPayload := `{"operationName":"VideoMetadata","variables":{"channelLogin":"","videoID":"%s"},"extensions":{"persistedQuery":{"version":1,"sha256Hash":"226edb3e692509f727fd56821f5653c05740242c82b0388883e0c0e75dcbf687"}}}`
+	body := strings.NewReader(fmt.Sprintf(gqlPayload, id))
+	req, err := http.NewRequest(http.MethodPost, c.apiURL, body)
 	if err != nil {
 		return VOD{}, errors.WithStack(err)
 	}
-	req.Header.Add("Client-ID", c.clientID)
-	resp, err := c.client.Do(req)
+	dump, err := httputil.DumpRequestOut(req, true)
 	if err != nil {
 		return VOD{}, errors.WithStack(err)
+	}
+	req.Header.Set("Client-Id", c.clientID)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return VOD{}, errors.Errorf("%v\n%s", err, string(dump))
 	}
 	defer resp.Body.Close()
 	if s := resp.StatusCode; s < 200 || s >= 300 {
-		b, _ := ioutil.ReadAll(resp.Body)
-		return VOD{}, errors.Errorf("%d\n%s\n%s", s, u, string(b))
+		return VOD{}, errors.Errorf("invalid status code %d\n%s", s, string(dump))
 	}
-	var d data
-	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
-		return VOD{}, errors.WithStack(err)
+
+	type respPayload struct {
+		Data struct {
+			Video struct {
+				Title string `json:"title"`
+			} `json:"video"`
+		} `json:"data"`
 	}
-	if len(d.Data) != 1 {
-		return VOD{}, errors.New("unexpected data length")
+
+	var p respPayload
+	if err := json.NewDecoder(resp.Body).Decode(&p); err != nil {
+		return VOD{}, errors.Errorf("%v\n%s", err, string(dump))
 	}
-	return d.Data[0], nil
+	return VOD{Title: p.Data.Video.Title}, nil
 }
